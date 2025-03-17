90e216f74d35de60b7233605d07b5971b578115a
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2025-03-17 17:19:26 -0400

    Split serialization test into two notebooks, support ufloats and dictionaries in h5 serializable files

--- src/utility/h5_serializable_file.py
+++ src/utility/h5_serializable_file.py
@@ -2,17 +2,48 @@ import h5py
 import numpy as np
 import os
 import inspect
+import json
+from uncertainties.core import Variable as UFloat
+from uncertainties import ufloat
 
 class H5Serializable:
     '''
     A class that can be serialized to/from an h5 file. Does not support groups.
     '''
+    class __JSONEncoder(json.JSONEncoder):
+        '''
+        JSON encoder that supports ufloats
+        '''
+        def default(self, obj):
+            if isinstance(obj, UFloat):
+                return {'__ufloat__': True, 'nominal_value': obj.nominal_value, 'std_dev': obj.std_dev}
+            return super().default(obj)
+    
+    class __JSONDecoder(json.JSONDecoder):
+        '''
+        JSON decoder that supports ufloats
+        '''
+        def __init__(self, *args, **kwargs):
+            json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+        def object_hook(self, d):
+            if "__ufloat__" in d:
+                return ufloat(float(d['nominal_value']), float(d['std_dev']))
+            return d
+    
     def load_from_path(self, file_path : str):
         try:
             hf = h5py.File(file_path, 'r')
 
             # TODO: Support recursion through groups
             for name, value in hf.attrs.items():
+                # Deserialize any dictionaries written to strings
+                if isinstance(value, str):
+                    if value.startswith("JSON"):
+                        value = json.loads(value[4:], cls=H5Serializable.__JSONDecoder)
+                    elif value.startswith("UFLOAT"):
+                        nominal_value, std_dev = value[6:].split("+/-")
+                        value = ufloat(float(nominal_value), float(std_dev))
+                    
                 self.__setattr__(name, value)
             for name, value in hf.items():
                 if isinstance(value, h5py.Dataset):
@@ -31,7 +62,7 @@ class H5Serializable:
         try:
             hf = h5py.File(file_path, 'w')
             # Filter out "private" attributes
-            names = [key for key in dir(self) if not key.startswith('__')]
+            names = [key for key in dir(self) if not key.startswith('_')]
             for name in names:
                 try:
                     value = self.__getattribute__(name)
@@ -39,6 +70,12 @@ class H5Serializable:
                     # frankly I don't even know what a np string is but they crop up sometimes
                     if isinstance(value, np.str_):
                         value = str(value)
+                    # dictionaries don't serialize either so we go through json
+                    elif isinstance(value, dict):
+                        value = "JSON" + json.dumps(value, cls=H5Serializable.__JSONEncoder)
+                    elif isinstance(value, UFloat):
+                        value = f"UFLOAT{value.nominal_value}+/-{value.std_dev}"
+                    
                     if isinstance(value, list) or isinstance(value, np.ndarray):
                         hf.create_dataset(name, data = value)
                     elif not inspect.ismethod(value):
