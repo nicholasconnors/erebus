30a54d979bc5fa5fa381f01146bfe4a52650c8fc
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2025-05-26 18:18:37 -0400

    workflow

--- .github/workflows/generate_schemas.py
+++ .github/workflows/generate_schemas.py
@@ -7,5 +7,5 @@ print(directory)
 from src.utility.planet import Planet
 from src.utility.run_cfg import ErebusRunConfig
 
-Planet.save_schema(directory + "/src/schema/planet_schema.json")
-ErebusRunConfig.save_schema(directory + "/src/schema/run_cfg_schema.json")
+Planet.save_schema(directory + "erebus/src/schema/planet_schema.json")
+ErebusRunConfig.save_schema(directory + "erebus/src/schema/run_cfg_schema.json")
