e5822b80365c98cebfc2ca3c06c89a8ff1ceb352
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2025-05-30 11:48:10 -0400

    Lessen python version requirement, make build script just use conda env and immediately install

--- build.sh
+++ build.sh
@@ -8,18 +8,6 @@ fi
 
 cd "build"
 
-venv_dir="./venv"
-
-if [ ! -d "$venv_dir" ]; then
-    echo "Creating virtual environment"
-    python3 -m venv "$venv_dir"
-    echo "Done"
-fi
-
-echo "Activating virtual environment"
-source "$venv_dir/bin/activate"
-echo "Done"
-
 echo "Installing depdencies"
 pip install --upgrade pip
 pip install setuptools
@@ -29,4 +17,5 @@ python -m pip install build
 python -m build --outdir ./dist ../
 
 echo "Done"
-deactivate
+
+pip install dist/*.tar.gz
