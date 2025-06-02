5488cce472a3a8d68226cb5a105f5d6b4f854612
Nicholas Connors <nicholas.connors@mail.mcgill.ca>
2025-06-02 16:31:51 -0400

    Update generate_schemas.py

--- .github/workflows/generate_schemas.py
+++ .github/workflows/generate_schemas.py
@@ -4,8 +4,8 @@ directory = os.path.abspath(os.path.dirname(os.getcwd()) + "/src/erebus")
 sys.path.append(directory)
 print(directory)
 
-from .utility.planet import Planet
-from .utility.run_cfg import ErebusRunConfig
+from utility.planet import Planet
+from utility.run_cfg import ErebusRunConfig
 
 Planet._save_schema(directory + "/erebus/src/erebus/schema/planet_schema.json")
 ErebusRunConfig._save_schema(directory + "/erebus/src/erebus/schema/run_cfg_schema.json")
