Fix period error propagation sign

src/erebus/individual_fit.py
@@ -61,7 +61,7 @@ class IndividualFit(H5Serializable):
         
         nominal_period = planet.p if isinstance(planet.p, float) else planet.p.nominal_value
         predicted_t_sec = (planet.t0 - np.min(photometry_data.time) - 2400000.5 + planet.p / 2.0) % nominal_period
-        number_of_periods = (planet.t0.nominal_value - np.min(photometry_data.time) - 2400000.5) / nominal_period
+        number_of_periods = np.abs(planet.t0.nominal_value - np.min(photometry_data.time) - 2400000.5) / nominal_period
         std_dev = np.sqrt(planet.t0.std_dev**2 + (number_of_periods * planet.p.std_dev)**2)
         predicted_t_sec.std_dev = std_dev
         