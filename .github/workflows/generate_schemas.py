5e7afb370d476c2db7eb016fb18302135611413e
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2025-06-12 15:54:08 -0400

    Possibly fix schema action

--- .github/workflows/generate_schemas.py
+++ .github/workflows/generate_schemas.py
@@ -1,16 +1,8 @@
 import sys
 import os
-directory = os.path.abspath(os.path.dirname(os.getcwd()) + "/erebus/src/erebus/utility")
-sys.path.append(directory)
-print(directory)
 
-print(os.path.isdir(directory))
-
-for item in os.listdir(directory):
-  print(item)
-
-from planet import Planet
-from run_cfg import ErebusRunConfig
+from erebus.utility.planet import Planet
+from erebus.utility.run_cfg import ErebusRunConfig
 
 Planet._save_schema(os.path.dirname(os.getcwd()) + "/erebus/src/erebus/schema/planet_schema.json")
 ErebusRunConfig._save_schema(os.path.dirname(os.getcwd()) + "/erebus/src/erebus/schema/run_cfg_schema.json")
