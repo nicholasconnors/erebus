3a26027c04749a229a3e0cbf1add6263bd0eee6c
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2025-06-15 10:39:18 -0400

    Add method for parsing lookup table

--- src/erebus/utility/planet.py
+++ src/erebus/utility/planet.py
@@ -11,6 +11,7 @@ from pydantic_yaml import parse_yaml_file_as
 from pydantic_yaml import to_yaml_file
 
 import numpy as np
+import os
 
 class Planet:
     '''
@@ -23,7 +24,7 @@ class Planet:
     Attributes:
         name (str): The name of the planet.
         t0 (UFloat | float): The last known time of conjunction of the planet.
-        t0_lookup_path (str): Optional replacement for t0, path to csv file with calculated TTVs (Two columns: t0 in BJD-2,450,000, error)
+        t0_lookup_path (str): Optional replacement for t0, relative path to csv file with calculated TTVs (Two columns: t0 in BJD-2,450,000, error)
         a_rstar (UFloat | float): The ratio of semi-major axis to star radius.
         p (UFloat | float): The period of the planet in days.
         rp_rstar (UFloat | float): The ratio of the planet's radius to the star's radius.
@@ -51,7 +52,7 @@ class Planet:
         Attributes:
             name            Name of the planet
             t0              Midpoint time of reference transit
-            t0_lookup_path  Optional replacement for t0, path to file with calculated TTVs (Two columns: t0, error)
+            t0_lookup_path  Optional replacement for t0, relative path to file with calculated TTVs (Two columns: t0, error)
             a_rstar         Semi-major axis in units of stellar radii
             p               Orbital period in days
             rp_rstar        Radius of the exoplanet in units of stellar radii
@@ -97,10 +98,27 @@ class Planet:
     
     def __init__(self, yaml_path : str):
         self.__load_from_yaml(parse_yaml_file_as(Planet._PlanetYAML, yaml_path))
+        self._path = yaml_path
     
     def _save_schema(path : str):
         planet_schema = Planet._PlanetYAML.model_json_schema()
         planet_schema_json = json.dumps(planet_schema, indent=2)
         with open(path, "w") as f:
             f.write(planet_schema_json)
+            
+    def get_closest_t0(self, obs_start):
+        '''Given a start time in BJD-2,450,000, use the lookup file to get the closest t0'''
+        if self.t0_lookup_path is None:
+            return self.t0
+        else:
+            path = self.t0_lookup_path
+            if not os.path.isabs(path):
+                folder = os.path.dirname(self._path)
+                path = folder + path
+
+            table = np.loadtxt(path, delimiter=',')
+            t0s = table[:,0]
+            lt_target = np.where(t0s < obs_start)
+            ind = np.argmax(lt_target)
+            return ufloat(table[ind,0], table[ind,1])
         
